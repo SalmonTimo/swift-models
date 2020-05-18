1c32be553b6939d5b84b1798e23e390ba1100498
Timothy Salmon <tsalmon2000@gmail.com>
2020-05-18 12:05:22 -0500

    gather flac and transcript files

--- Datasets/LibriSpeech/LibriSpeech.swift
+++ Datasets/LibriSpeech/LibriSpeech.swift
@@ -16,14 +16,35 @@ func fetchLibriSpeechDataset(
   guard let remoteRoot = URL(string: remoteBaseDirectory) else {
     fatalError("Failed to create LibriSpeech root url: \(remoteBaseDirectory)")
   }
-  let LibriSpeechDataset = DatasetUtilities.download(
+  let datasetRoot = DatasetUtilities.download(
     filename: filename,
     fileExtension: "tar.gz",
     remoteRoot: remoteRoot,
     localStorageDirectory: localStorageDirectory)
 
+  let (soundFiles, transcripts) = fetchFiles(from: datasetRoot)
+  
   // TODO: Walk directory, unpack .flac files to spectrogram and align with transcript
+  audio, transcripts = fetchFLACFiles(fromRoot: datasetRoot)
   }
 
   fatalError("Incomplete implementation.")
+}
+
+func fetchFiles(from root: URL
+) -> (flacURLs: [URL], transcripts: [URL]) {
+  let localFileManager = FileManager()
+  let resourceKeys = Set<URLResourceKey>([.nameKey, .isDirectoryKey])
+  let directoryEnumerator = localFileManager.enumerator(at: root, includingPropertiesForKeys: Array(resourceKeys), options: .skipsHiddenFiles)!
+  var flacURLs: [URL] = []
+  var transcriptURLs: [URL] = []
+  for case let fileURL as URL in directoryEnumerator {
+    if fileURL.absoluteString.hasSuffix(".flac") {
+      flacURLs.append(fileURL)
+    }
+    if fileURL.absoluteString.hasSuffix(".trans.txt") {
+      transcriptURLs.append(fileURL)
+    }
+  }
+  return (flacURLs, transcriptURLs)
 }
